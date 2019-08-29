96a1829a09fb4e251a289a58b0e2c9b66b533164
Constantine <ceeveeya@gmail.com>
2019-08-29 18:32:20 +0300

    Nest user serializer to post response author

--- src/apps/backend/serializers/posts.py
+++ src/apps/backend/serializers/posts.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from src.apps.backend.models import Post
+from src.apps.backend.serializers.user import UserResponseSerializer
 
 
 class PostResponseSerializer(serializers.ModelSerializer):
+    author = UserResponseSerializer(read_only=True)
+
     class Meta:
         model = Post
         fields = read_only_fields = (
