b65b9062dd4ad9ade7dac60269f7ce7d0827fb9c
Constantine <ceeveeya@gmail.com>
2019-08-29 08:37:00 +0300

    Make user email field unique

--- src/apps/backend/models/user.py
+++ src/apps/backend/models/user.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
+    email = models.EmailField(
+        verbose_name=_('Email'),
+        blank=False,
+        unique=True,
+    )
+
     class Meta:
         verbose_name = _('User')
         verbose_name_plural = _('Users')
