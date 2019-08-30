15fa8f961c4f4f936c98d86f8004568a5d95611e
Constantine <ceeveeya@gmail.com>
2019-08-30 11:22:47 +0300

    Bot can perform liking by rules

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -77,6 +77,73 @@ class Bot:
                 user_data['total_posts'] = total_posts
                 log.info(f'Created {total_posts} posts as {username}')
 
+    def perform_liking(self):
+        log.info('Performing liking')
+        # Next user to perform a like is the user who has most posts
+        sorted_users = sorted(
+            self.users,
+            key=lambda user: self.users.get(user).get('total_posts'),
+            reverse=True
+        )
+        for user in sorted_users:
+            likes_count = 0
+            liked_posts = []  # Remember the posts user liked
+            likes_goal = self.faker.random_int(1, self.MAX_LIKES_PER_USER, 1)
+
+            # Auth and utility stuff
+            token = self.users.get(user).get('token')
+            headers = {'Authorization': f'Bearer {token}'}
+            error = False  # If something goes wrong, we're not stuck in a loop
+
+            friends = list(self.users.keys())  # Friends to like their posts
+            friends.remove(user)  # Don't like own posts
+            while friends and likes_count <= likes_goal and not error:
+                # Randomly chose a friend to like his posts
+                friend = self.faker.random_choices(friends, 1)[0]
+                response = requests.get(
+                    f'{SERVER_URL}/{friend}/posts/', headers=headers
+                )
+                if not response.ok:
+                    log.error(f'Error getting posts by {friend}')
+                    friends.remove(friend)
+                    continue
+
+                friend_posts = response.json()
+                # Like posts by friends who have at least one post with 0 likes
+                if all([post.get('likes_count') > 0 for post in friend_posts]):
+                    log.info(f'Every post by {friend} has at least one like')
+                    friends.remove(friend)
+                    continue
+
+                while likes_count <= likes_goal and friend_posts and not error:
+                    # Randomly choose a post to like
+                    post = self.faker.random_choices(friend_posts, 1)[0]
+                    post_id = post.get('id')
+                    # Don't like a post user already seen
+                    friend_posts.remove(
+                        [post for post in friend_posts
+                         if post.get('id') == post_id][0]
+                    )
+                    # Don't like a post twice
+                    if post_id in liked_posts:
+                        continue
+                    response = requests.post(
+                        f'{SERVER_URL}/{friend}/posts/{post_id}/like/',
+                        headers=headers
+                    )
+                    if not response.ok:
+                        log.error(f'Error liking post {post_id}')
+                        error = True
+                        continue
+
+                    likes_count += 1
+                    liked_posts.append(post_id)
+                    log.info(
+                        f'{user} liked post {post_id}'
+                        f' by {post.get("author").get("username")}'
+                        f' it now has {post.get("likes_count") + 1} likes'
+                    )
+
 
 if __name__ == '__main__':
     bot = Bot()
@@ -90,3 +157,4 @@ if __name__ == '__main__':
     bot.create_users()
     bot.get_tokens()
     bot.perform_posting()
+    bot.perform_liking()
